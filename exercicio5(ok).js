3d2b0e8ea2d60ec9bba2451a9e01762b0907c477
Higor Costa <hscprg@gmail.com>
2015-05-05 22:56:59 -0300

    exercicio javascript
    ultimo commit

--- exercicio5(ok).js
+++ exercicio5(ok).js
@@ -1,3 +1,16 @@
 /*05 - Baseado nos conceitos aprendidos de O.O. em javascript crie uma Classe chamada Empregado, que tem três atributos: nome, sobrenome e salarioMensal. A classe também deve conter dois métodos calcularSalarioAno que retorna o salário mensal multiplicado por 12 e dadosEmpregado que retorna os dados seguido este modelo: “Carlos Silva, Salário: 120.5”
 */
 
+var Empregado = function(nome1, sobrenome1, sal){
+    var nome            = nome1;
+    var sobrenome       = sobrenome1;
+    var salarioMensal   = sal;
+
+    this.calcularSalarioAno = function(){
+        return salarioMensal * 12;
+    }
+
+    this.dadosEmpregado = function(){
+        return nome + " " + sobrenome + ", Salário: " + salarioMensal;
+    }
+};
